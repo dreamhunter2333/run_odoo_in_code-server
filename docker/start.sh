898a884aabacbb483d957a29aa3b5215a0ad7b06
jinmu333 <jinmu333@qq.com>
2019-11-12 17:01:11 +0000

    add dependence buliding vscode

--- docker/start.sh
+++ docker/start.sh
@@ -9,6 +9,10 @@ then
     then
     	echo "odoo coder-server is downing";
     	docker-compose -f docker-compose.yml down;
+    elif [ $1 == "build" ]
+    then
+	echo "odoo coder-server is building";
+	docker build -f Dockerfile -t odoo13_code_server:1.0 .;
     else
 	echo "参数错误";
     fi
